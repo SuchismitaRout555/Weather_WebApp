965e9c3d6e9780c808ac012e0037019c39111e59
Suchismita Rout <97534398+SuchismitaRout555@users.noreply.github.com>
2024-03-29 23:24:33 +0530

    allow location

--- index.js
+++ index.js
@@ -63,3 +63,24 @@ function switchTab(clickedTab){
 
     }
 }
+
+//allow your location 
+
+function getLocation(){
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+
+    }
+    else{
+        console.log("No geo Location Support");
+    }
+}
+
+function showPosition(position){
+    let lat =position.coords.latitude;
+    let longi = position.coords.longitude;
+
+
+    console.log(lat);
+    console.log(longi);
+}
