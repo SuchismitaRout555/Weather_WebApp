caf734c57230beabc9beb410a927bf83e3f36749
Suchismita Rout <97534398+SuchismitaRout555@users.noreply.github.com>
2024-04-01 02:00:43 +0530

    added js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ const userTab = document.querySelector("[data-userWeather]");
 const searchTab = document.querySelector("[data-searchWeather]");
 const userContainer = document.querySelector(".weather-container");
 const grantAccessContainer = document.querySelector(".grant-loction-container");
-const searchForm = document.querySelector("[data-searchFom]");
+const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loding-container");
 const userInfoContainer = document.querySelector(".user-info-container");
 
@@ -158,7 +158,7 @@ async function fetchUserWeatherInfo(coordinates) {
     // api call
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`);
-        const data =  await response.json();
+        const data = await response.json();
 
         loadingScreen.classList.remove("active");
         userInfoContainer.classList.add("active");
@@ -172,14 +172,85 @@ async function fetchUserWeatherInfo(coordinates) {
 
 }
 
-function renderWeatherInfo(weatherInfo){
+function renderWeatherInfo(weatherInfo) {
     const cityName = document.querySelector("[data-cityName]");
-    const countryIcon= document.querySelector("[ data-countryIcon]");
+    const countryIcon = document.querySelector("[ data-countryIcon]");
     const desc = document.querySelector("[data-weatherDesc]");
     const weatherIcon = document.querySelector("[data-weatherIcon]");
     const temp = document.querySelector("[data-temp]");
     const windspeed = document.querySelector("[data-windspeed]");
     const humidity = document.querySelector("[data-humidity]");
     const cloudness = document.querySelector("[data-cloudiness]");
+
+    //fetch valiues from weatherinfo onjecy and put it ui elements
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
+    weatherIcon.src = `https://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+    temp.innerText = weatherInfo?.main?.temp;
+    windspeed.innerText = weatherInfo?.wind?.speed;
+    humidity.innerText = weatherInfo?.main?.humidity;
+    cloudness.innerText = weatherInfo?.clouds?.all;
+
+
+
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        function showAlert() {
+            alert(" no Geolocation support available");
+        }
+
+    }
+}
+
+function showPosition(position) {
+    const userCoordinates = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+
+
+    }
+    sessionStorage.setItem("userCoordinates", JSON.stringify(userCoordinates));
+    fetchUserWeatherInfo(userCoordinates);
+}
+
+//grant Access buttom ke litener lagana padega like jab click ho tab live loaction acess kare andstore kare 
+
+const grantAccessButton = document.querySelector("[data-grantAccess]");
+grantAccessButton.addEventListener('click', getLocation);
+
+const searchInput = document.querySelector("[data-searchInput]");
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let cityName = searchInput.ariaValueMax;
+
+    if (cityName === "")
+        return;
+    else
+        fetchSearchWeatherInfo(cityName);
+
+
+})
+async function fetchUserWeatherInfo(city) {
+    loadingScreen.classList.add("active");
+    userInfoContainer.classList.remove("active");
+    grantAccessContainer.classList.remove("active");
+
+    try {
+        const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+
+        const data = await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch (err) {
+
+    }
 
 }
